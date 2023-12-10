e61ad116b98c79daf2c031c7c1e2656d7a5a6dd9
Colin <colinclissonhg@gmail.com>
2023-12-10 15:27:10 -0500

    feat: add fallback when no image

--- components/BookmarkItem.tsx
+++ components/BookmarkItem.tsx
@@ -1,5 +1,6 @@
 import { Bookmark } from '@/types/bookmark';
 import Link from 'next/link';
+import { MdOutlineHideImage } from 'react-icons/md';
 
 interface Props {
     bookmark: Omit<Bookmark, 'id'>
@@ -17,11 +18,22 @@ export default function BookmarkItem({ bookmark }: Props) {
         active:bg-white/20 md:hover:bg-white/20
       "
     >
-      <img
-        src={thumbnail}
-        alt={bookmark.title}
-        className="w-[70px] aspect-square object-cover rounded-xl"
-      />
+      {
+        thumbnail ? (
+          <img
+            src={thumbnail}
+            alt={bookmark.title}
+            className="w-[70px] aspect-square object-cover rounded-xl"
+          />
+        ) : (
+          <div className="flex items-center justify-center w-[70px] aspect-square bg-white/10 rounded-xl">
+            <MdOutlineHideImage
+              size={50}
+              className="opacity-50"
+            />
+          </div>
+        )
+      }
       <div className="flex-1 overflow-hidden">
         <p className="font-bold truncate mb-1">{bookmark.title}</p>
         <p className="text-sm opacity-80 line-clamp-2">{bookmark.description}</p>
