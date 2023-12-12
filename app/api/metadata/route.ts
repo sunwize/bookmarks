d87fdfa02b131523139d2d71ec43009ee292dbd8
Colin <colinclissonhg@gmail.com>
2023-12-12 18:30:22 -0500

    refactor: major changes on metadata extraction logic

--- app/api/metadata/route.ts
+++ app/api/metadata/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import puppeteer from 'puppeteer-core';
-// @ts-ignore
-import * as chromium from 'chromium';
+import { extractMetadata } from '@/lib/services/metadata';
 
 export const GET = async (request: NextRequest) => {
   const url = request.nextUrl.searchParams.get('url');
@@ -12,60 +10,7 @@ export const GET = async (request: NextRequest) => {
     });
   }
 
-  if (!chromium.path) {
-    await chromium.install();
-  }
+  const metadata = await extractMetadata(url);
 
-  const browser = await puppeteer.launch({
-    args: ['--hide-scrollbars', '--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: chromium.path,
-    headless: 'new',
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
-
-  await page.goto(url);
-  await page.waitForSelector('head');
-  const metadata = await page.evaluate(() => {
-    const extractTitle = () => {
-      return document.head.querySelector('meta[property="og:title"]')?.getAttribute('content')
-        || document.querySelector('meta[name="title"]')?.getAttribute('content');
-    };
-    const extractDescription = () => {
-      return document.head.querySelector('meta[property="og:description"]')?.getAttribute('content')
-        || document.querySelector('meta[name="description"]')?.getAttribute('content');
-    };
-    const extractImage = () => {
-      return document.head.querySelector('meta[property="og:image"]')?.getAttribute('content')
-        || document.querySelector('#landingImage')?.getAttribute('src') // Amazon
-        || document.querySelector('picture img')?.getAttribute('src'); // TikTok
-    };
-    const extractFavicon = () => {
-      return `https://www.google.com/s2/favicons?domain=${window.location.hostname}&sz=256`;
-    };
-    const extractSitename = () => {
-      const tab = window.location.hostname.split('.');
-      const str = tab[tab.length - 2];
-      const sitename = str.charAt(0).toUpperCase() + str.slice(1);
-
-      return document.head.querySelector('meta[property="og:site_name"]')?.getAttribute('content')
-        || sitename;
-    };
-
-    return {
-      title: extractTitle(),
-      description: extractDescription(),
-      image_url: extractImage() || extractFavicon(),
-      favicon: extractFavicon(),
-      sitename: extractSitename(),
-      domain: window.location.hostname,
-    };
-  });
-
-  await browser.close();
-
-  return NextResponse.json({
-    ...metadata,
-    url,
-  });
+  return NextResponse.json(metadata);
 };
---
+++ lib/services/metadata/extractors/tiktok.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Bookmark } from '@/types/bookmark';
+
+interface Response {
+    title: string
+    description: string
+    author_name: string
+    thumbnail_url: string
+    provider_url: string
+    provider_name: string
+    hostname: string
+}
+
+export const extractTikTokMetadata = async (url: string): Promise<Omit<Bookmark, 'id'>> => {
+  const { data } = await axios.get<Response>('https://www.tiktok.com/oembed', {
+    params: { url },
+  });
+
+  const domainUrl = new URL(data.provider_url);
+
+  return {
+    title: data.author_name,
+    description: data.title,
+    image_url: data.thumbnail_url,
+    sitename: data.provider_name,
+    domain: domainUrl.hostname,
+    url,
+  };
+};
