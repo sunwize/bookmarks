55605d7660920b65d4ec570d1f0128677c25ea09
Clisson <cclisson@eyeinmedia.com>
2024-01-10 23:38:48 -0500

    feat: add delete action

--- components/drawers/CollectionActions.tsx
+++ components/drawers/CollectionActions.tsx
@@ -1,9 +1,11 @@
 import Drawer from '@/components/ui/Drawer';
 import { BookmarkCollection } from '@/types/bookmark';
 import Button from '@/components/ui/Button';
-import { FiEdit3, FiUserPlus } from 'react-icons/fi';
+import { FiEdit3, FiTrash, FiUserPlus } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { getAuthenticatedUser } from '@/lib/utils/auth';
 
-export type CollectionActionType = 'edit' | 'add-user';
+export type CollectionActionType = 'edit' | 'add-user' | 'delete';
 
 type Props = {
   visible: boolean
@@ -12,33 +14,61 @@ type Props = {
   onAction: (type: CollectionActionType) => void
 }
 
-const ActionButton = ({ onClick, children }: { onClick: () => void, children: React.ReactNode }) => (
+const ActionButton = (
+  { onClick, className, children }:
+  { onClick: () => void, className?: string, children: React.ReactNode }
+) => (
   <Button
     onClick={onClick}
-    className="w-full flex items-center gap-4 text-left text-xl bg-white/20 text-white font-black py-4 px-6"
+    className={`w-full flex items-center gap-4 text-left text-xl bg-white/20 text-white font-black py-4 px-6 ${className}`}
   >
     {children}
   </Button>
 );
 
 export default function CollectionActions({ visible, onHide, collection, onAction }: Props) {
+  const [isOwner, setIsOwner] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const user = await getAuthenticatedUser();
+      setIsOwner(user.id === collection.user_id || !collection.user_id);
+    })();
+  }, [collection]);
+
   return (
     <Drawer
       visible={visible}
       onHide={onHide}
     >
-      <p className="text-3xl font-bold truncate pt-3 px-2">{collection.title}</p>
-      <hr className="border-white/40 my-3"/>
-      <div className="grid grid-cols-1 gap-2 px-2 pb-3">
+      <p className="text-3xl font-bold truncate px-2 md:px-3 py-3">{collection.title}</p>
+      <hr className="border-white/40"/>
+      <div className="grid grid-cols-1 gap-2 px-2 md:px-3 py-3">
         <ActionButton onClick={() => onAction('edit')}>
-          <FiEdit3 size={22} />
+          <FiEdit3 size={22}/>
           <span>Edit</span>
         </ActionButton>
         <ActionButton onClick={() => onAction('add-user')}>
-          <FiUserPlus size={22} />
+          <FiUserPlus size={22}/>
           <span>Share with someone</span>
         </ActionButton>
       </div>
+      {
+        isOwner && (
+          <>
+            <hr className="border-white/40"/>
+            <div className="grid grid-cols-1 gap-2 px-2 md:px-3 py-3">
+              <ActionButton
+                onClick={() => onAction('delete')}
+                className="!bg-red-500"
+              >
+                <FiTrash size={22}/>
+                <span>Delete</span>
+              </ActionButton>
+            </div>
+          </>
+        )
+      }
     </Drawer>
   );
 }
