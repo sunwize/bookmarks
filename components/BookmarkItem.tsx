0b337195e6fb7e59ef242e575a6c9d8dff0c2a45
Colin <colinclissonhg@gmail.com>
2024-01-14 16:12:29 -0500

    feat: use bookmark image component

--- components/BookmarkItem.tsx
+++ components/BookmarkItem.tsx
@@ -1,27 +1,13 @@
 import { Bookmark } from '@/types/bookmark';
 import Link from 'next/link';
-import { MdOutlineHideImage } from 'react-icons/md';
-import Image from 'next/image';
-import { useRef } from 'react';
+import BookmarkImage from '@/components/BookmarkImage';
 
 type Props = {
   bookmark: Omit<Bookmark, 'id'>
-  onLoadImageError?: () => void
+  onLoadImageError?: (newImageUrl: string) => void
 }
 
 export default function BookmarkItem({ bookmark, onLoadImageError }: Props) {
-  const thumbnail = bookmark.image_url;
-  const isLoadError = useRef(false);
-
-  const _onLoadImageError = () => {
-    if (isLoadError.current) {
-      return;
-    }
-
-    isLoadError.current = true;
-    onLoadImageError?.();
-  };
-
   return (
     <Link
       href={bookmark.url}
@@ -31,26 +17,10 @@ export default function BookmarkItem({ bookmark, onLoadImageError }: Props) {
         active:bg-white/20 md:hover:bg-white/20
       "
     >
-      {
-        thumbnail ? (
-          <Image
-            onError={_onLoadImageError}
-            src={thumbnail}
-            alt={bookmark.title}
-            width={150}
-            height={150}
-            quality={100}
-            className="w-[70px] aspect-square object-cover rounded-xl"
-          />
-        ) : (
-          <div className="flex items-center justify-center w-[70px] aspect-square bg-white/10 rounded-xl">
-            <MdOutlineHideImage
-              size={50}
-              className="opacity-50"
-            />
-          </div>
-        )
-      }
+      <BookmarkImage
+        bookmark={bookmark}
+        onError={onLoadImageError}
+      />
       <div className="flex-1 overflow-hidden">
         <p className="font-bold truncate mb-1">{bookmark.title}</p>
         <p className="text-sm opacity-80 line-clamp-2">{bookmark.description}</p>
